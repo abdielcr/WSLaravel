ce27afa10d854ea361fe6fff3c097ea624294368
Abdiel <acrojas@blitzsoftware.com.mx>
2019-07-05 17:50:31 -0700

    WSL002 - Agregando Rutas personalizadas

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,22 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Retornando un valor dentro de una url pero sin vitas definida.
+Route::get('/wslaravel', function(){
+    return 'WS-LARAVEL';
+});
+
+//Retornando directamente un Json a la vista.
+Route::get('/wsjson',function(){
+    return [
+        'Nombre'  => 'Abdiel Carrasco',
+        'Puesto'  => 'Software Developer',
+        'Empresa' => 'C4',
+    ];
+});
+
+//Ruta con vista creada
+Route::get('/wsvistalaravel',function(){
+    return view('wsvistalaravel');//Nombre del Archivo
+});
