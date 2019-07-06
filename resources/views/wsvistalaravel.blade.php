15b760ed6f49c40eb087b6b91a8288c69fcdffef
Abdiel <acrojas@blitzsoftware.com.mx>
2019-07-05 18:19:08 -0700

    VJ003 - ConexionWs y archivos en public

--- resources/views/wsvistalaravel.blade.php
+++ resources/views/wsvistalaravel.blade.php
@@ -1,99 +1,220 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Video JS</title>
+  <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700" rel="stylesheet">
+  <link rel="stylesheet" href="css/home.css">
+  <link rel="stylesheet" href="css/modal.css">
+  <!-- <link rel="stylesheet" href="../resources/css/home.css" />
+  <link rel="stylesheet" href="../resources/css/modal.css" /> -->
+</head>
+<body>
+  <section class="home" id="home">
+    <div class="home-sidebar">
+      <div class="sidebar">
+        <div class="logo">
+          <figure class="logo-image">
+            <img src="images/logoVid.png" alt="Platzi Video - la Plataforma de videos cheveres"/>
+          </figure>
         </div>
-    </body>
+        <div class="sidebarPlaylist">
+          <h2 class="sidebarPlaylist-title">Mis Reproducciones <span class="btn warning"></br> Mirar Playlist</span> </h2>
+          <ol class="myPlaylist">
+            <li class="myPlaylist-item">
+              <a href="#">
+                <span>
+                  Reasons To Choose A Notebook Over A Computer Desktop
+                </span>
+              </a>
+            </li>
+            <li class="myPlaylist-item">
+              <a href="#">
+                <span>
+                  Programming
+                </span>
+              </a>
+            </li>
+            <li class="myPlaylist-item">
+              <a href="#">
+                <span>
+                  Programmin POO
+                </span>
+              </a>
+            </li>
+            <li class="myPlaylist-item">
+              <a href="#">
+                <span>
+                  El Padrino
+                </span>
+              </a>
+            </li>
+            <li class="myPlaylist-item">
+              <a href="#">
+                <span>
+                  Marte
+                </span>
+              </a>
+            </li>
+            <li class="myPlaylist-item">
+              <a href="#">
+                <span>
+                 Terra Nova
+                </span>
+              </a>
+            </li>
+            <li class="myPlaylist-item">
+              <a href="#">
+                <span>
+                 Odisea
+                </span>
+              </a>
+            </li>
+            <li class="myPlaylist-item">
+              <a href="#">
+                <span>
+                  Apolo 13
+                </span>
+              </a>
+            </li>
+            <li class="myPlaylist-item">
+              <a href="#">
+                <span>
+                  Predator
+                </span>
+              </a>
+            </li>
+          </ol>
+        </div>
+        <div class="sidebarPlaylist">
+          <h2 class="sidebarPlaylist-title">Reproducciones de Amigos</h2>
+          <ul class="playlistFriends">
+            <li class="playlistFriends-item">
+              <a href="#">
+                <img src="images/covers/robert.jpg" alt="echame la culpa" />
+                <span>
+                  Alberto Robles
+                </span>
+              </a>
+            </li>
+            <li class="playlistFriends-item">
+              <a href="#">
+                <img src="images/covers/katerine.jpg" alt="echame la culpa" />
+                <span>
+                 Marian robles
+                </span>
+              </a>
+            </li>
+            <li class="playlistFriends-item">
+              <a href="#">
+                <img src="images/covers/karen.jpg" alt="echame la culpa" />
+                <span>
+                  Karen Rodriguez
+                </span>
+              </a>
+            </li>
+            <li class="playlistFriends-item">
+              <a href="#">
+                <img src="images/covers/esmeralda.jpg" alt="echame la culpa" />
+                <span>
+                  Esmeralda Mitre
+                </span>
+              </a>
+            </li>
+            <li class="playlistFriends-item">
+              <a href="#">
+                <img src="images/covers/jose.jpg" alt="echame la culpa" />
+                <span>
+                  Javier Perez
+                </span>
+              </a>
+            </li>
+            <li class="playlistFriends-item">
+              <a href="#">
+                <img src="images/covers/betsy.jpg" alt="echame la culpa" />
+                <span>
+                  Betsy Heredia
+                </span>
+              </a>
+            </li>
+            <li class="playlistFriends-item">
+              <a href="#">
+                <img src="images/covers/charlie.jpg" alt="echame la culpa" />
+                <span>
+                  Carlos Gonzalez
+                </span>
+              </a>
+            </li>
+            <li class="playlistFriends-item">
+              <a href="#">
+                <img src="images/covers/maquenzie.jpg" alt="echame la culpa" />
+                <span>
+                  Vanesa Flores
+                </span>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+    </div>
+    <div class="home-primary">
+      <!-- <section class="primary"> -->
+        <div class="primary-search">
+          <form action="" class="search" id="form">
+            <input type="text" name="name" placeholder="Buscar un artista o tema favorito"/>
+          </form>
+        </div>
+        <div class="primary-user">
+          <div class="user">
+            <p>
+              <img src="images/covers/Abdiel.png" alt=""/>
+              <span>Abdiel Carrasco</span>
+            </p>
+          </div>
+        </div>
+        <div class="primary-list">
+          <div class="primaryPlaylist">
+            <h3 class="primaryPlaylist-topic">A los golpes</h3>
+            <h2 class="primaryPlaylist-title">Acción</h2>
+            <div class="primaryPlaylist-list" id="action">
+              <img src="src/images/loader.gif" width="50" height="50" alt="">
+            </div>
+          </div>
+          <div class="primaryPlaylist">
+            <h3 class="primaryPlaylist-topic">Para llorar</h3>
+            <h2 class="primaryPlaylist-title">Drama</h2>
+            <div class="primaryPlaylist-list" id="drama">
+              <img src="src/images/loader.gif" width="50" height="50" alt="">
+            </div>
+          </div>
+          <div class="primaryPlaylist">
+            <h3 class="primaryPlaylist-topic">Dibujitos animados</h3>
+            <h2 class="primaryPlaylist-title">Animación</h2>
+            <div class="primaryPlaylist-list" id="animation">
+              <img src="src/images/loader.gif" width="50" height="50" alt="">
+            </div>
+          </div>
+        </div>
+      <!-- </section> -->
+    </div>
+    <div class="home-featuring" id="featuring">
+
+    </div>
+  </section>
+  <div class="overlay" id="overlay">
+  </div>
+  <div class="modal" id="modal">
+    <h1>Título de pelicula</h1>
+    <div class="modal-content">
+      <img src="" alt="" width="170" height="256">
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione impedit maiores enim alias ex accusantium quasi cum autem, nam, voluptas tenetur laudantium quod! Odit voluptate illo, voluptas vel quia, quae.</p>
+    </div>
+    <div class="modal-buttons">
+      <button class="modal-btn primary" id="hide-modal">Cerrar Modal</button>
+    </div>
+  </div>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="js/home.js"></script>
+</body>
 </html>
